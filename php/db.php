676243d0ff7e595a7bc88bf7329d5b68d2ba3452
Zawoj <zawojskikacperkontakt@gmail.com>
2023-06-07 22:31:26 +0200

    my stuff

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "my_secret_pw_shh";
 $database = "l&l";
 $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
